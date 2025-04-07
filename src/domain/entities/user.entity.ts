6ca360922d5335fb8e42b4acca32fec51bdd32a5
JhonatanAM <jhonatan.mejiamendo@gmail.com>
2025-04-07 16:09:04 -0500

    added clean arquitecture folders

---
+++ src/domain/entities/user.entity.ts
@@ -0,0 +1,9 @@
+export class User{
+    constructor(
+        public readonly: string,
+        public name: string,
+        public email: string,
+        public groupId: string,
+        public password: string
+    ){}
+}
